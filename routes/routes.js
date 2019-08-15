6665b42247e9e7ae3ca97442b2dae3653451e81e
Muttakin Islam <islammuttakin@live.com>
2019-08-15 13:48:43 +0600

    get

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,11 @@ module.exports = function(app) {
 
   var app1 = require('./../controllers/controllers.js');
 
-  app.get('/index', app1.new);
-  app.get('/map', app1.new1);
-  app.get('/data', app1.new2);
+  app.get('/', app1.new);
+  
+  app.get('/index.html', app1.new);
+  app.get('/map.html', app1.new1);
+  app.get('/data.html', app1.new2);
 
 
 }
